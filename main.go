5b14236e8eec51768883b1beef5cb61cc53f0d8c
Joe Lanford <joe.lanford@gmail.com>
2020-06-02 10:44:26 -0400

    main.go: support (but deprecated) OPERATOR_NAME for leader lock, add --leader-election-namespace

--- main.go
+++ main.go
@@ -40,9 +40,10 @@ var (
 
 func main() {
 	var (
-		metricsAddr          string
-		enableLeaderElection bool
-		leaderElectionID     string
+		metricsAddr             string
+		enableLeaderElection    bool
+		leaderElectionID        string
+		leaderElectionNamespace string
 
 		watchesFile                    string
 		defaultMaxConcurrentReconciles int
@@ -55,21 +56,18 @@ func main() {
 	pflag.StringVar(&metricsAddr, "metrics-addr", "0.0.0.0:8383", "The address the metric endpoint binds to.")
 	pflag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
-	pflag.StringVar(&leaderElectionID, "leader-election-id", "leader-lock",
+	pflag.StringVar(&leaderElectionID, "leader-election-id", "",
 		"Name of the configmap that is used for holding the leader lock.")
+	pflag.StringVar(&leaderElectionNamespace, "leader-election-namespace", "",
+		"Namespace in which to create the leader election configmap for holding the leader lock (required if running locally).")
 
 	pflag.StringVar(&watchesFile, "watches-file", "./watches.yaml", "Path to watches.yaml file.")
 	pflag.DurationVar(&defaultReconcilePeriod, "reconcile-period", 0, "Default reconcile period for controllers (use 0 to disable periodic reconciliation)")
 	pflag.IntVar(&defaultMaxConcurrentReconciles, "max-concurrent-reconciles", 1, "Default maximum number of concurrent reconciles for controllers.")
 
-	// Deprecated: --max-workers flag is currently supported, but it does not align well with the name of the option it
-	//   configures on the controller (MaxConcurrentReconciles). Flag `--max-concurrent-reconciles` should be used
-	//   instead.
+	// Deprecated: --max-workers flag does not align well with the name of the option it configures on the controller
+	//   (MaxConcurrentReconciles). Flag `--max-concurrent-reconciles` should be used instead.
 	pflag.IntVar(&defaultMaxWorkers, "max-workers", 1, "Default maximum number of concurrent reconciles for controllers.")
-	if err := pflag.CommandLine.MarkDeprecated("max-workers", "use --default-max-concurrent-reconciles instead"); err != nil {
-		setupLog.Error(err, "failed to mark --max-workers flag as deprecated")
-		os.Exit(1)
-	}
 	if err := pflag.CommandLine.MarkHidden("max-workers"); err != nil {
 		setupLog.Error(err, "failed to hide --max-workers flag")
 		os.Exit(1)
@@ -86,18 +84,34 @@ func main() {
 		zapl.StacktraceLevel(&sttLvl),
 	))
 
+	// Deprecated: --max-workers flag does not align well with the name of the option it configures on the controller
+	//   (MaxConcurrentReconciles). Flag `--max-concurrent-reconciles` should be used instead.
 	if pflag.Lookup("max-workers").Changed {
-		if !pflag.Lookup("max-concurrent-reconciles").Changed {
+		setupLog.Info("flag --max-workers has been deprecated, use --max-concurrent-reconciles instead")
+		if pflag.Lookup("max-concurrent-reconciles").Changed {
+			setupLog.Info("ignoring --max-workers since --max-concurrent-reconciles is set")
+		} else {
 			defaultMaxConcurrentReconciles = defaultMaxWorkers
 		}
-		setupLog.Info("ignoring --max-workers since --max-concurrent-reconciles is set")
+	}
+
+	// Deprecated: OPERATOR_NAME environment variable is an artifact of the legacy operator-sdk project scaffolding.
+	//   Flag `--leader-election-id` should be used instead.
+	if operatorName, found := os.LookupEnv("OPERATOR_NAME"); found {
+		setupLog.Info("environment variable OPERATOR_NAME has been deprecated, use --leader-election-id instead.")
+		if pflag.Lookup("leader-election-id").Changed {
+			setupLog.Info("ignoring OPERATOR_NAME environment variable since --leader-election-id is set")
+		} else {
+			leaderElectionID = operatorName
+		}
 	}
 
 	options := ctrl.Options{
-		MetricsBindAddress: "0.0.0.0:8383",
-		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   leaderElectionID,
-		NewClient:          manager.NewDelegatingClientFunc(),
+		MetricsBindAddress:      "0.0.0.0:8383",
+		LeaderElection:          enableLeaderElection,
+		LeaderElectionID:        leaderElectionID,
+		LeaderElectionNamespace: leaderElectionNamespace,
+		NewClient:               manager.NewDelegatingClientFunc(),
 	}
 	manager.ConfigureWatchNamespaces(&options, setupLog)
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), options)
