2b65cbf0fef15bf6c88ebf1f2d205ed592d045d2
varshaprasad96 <varshaprasad96@gmail.com>
2022-01-21 14:40:48 -0800

    update scaffolding for 0.0.9 release

--- pkg/plugins/hybrid/v1alpha/scaffolds/init.go
+++ pkg/plugins/hybrid/v1alpha/scaffolds/init.go
@@ -34,19 +34,19 @@ import (
 
 const (
 	// ControllerRuntimeVersion is the kubernetes-sigs/controller-runtime version to be used in the project
-	ControllerRuntimeVersion = "v0.10.0"
+	ControllerRuntimeVersion = "v0.11.0"
 	// ControllerToolsVersion is the kubernetes-sigs/controller-tools version to be used in the project
-	ControllerToolsVersion = "v0.6.2"
+	ControllerToolsVersion = "v0.8.0"
 	// KustomizeVersion is the kubernetes-sigs/kustomize version to be used in the project
 	KustomizeVersion = "v3.8.7"
 
 	imageName = "controller:latest"
 
 	// TODO: This is a placeholder for now. This would probably be the operator-sdk version
-	hybridOperatorVersion = "0.0.8"
+	hybridOperatorVersion = "0.0.9"
 
 	// helmPluginVersion is the operator-framework/helm-operator-plugin version to be used in the project
-	helmPluginVersion = "v0.0.8"
+	helmPluginVersion = "v0.0.9"
 )
 
 var _ plugins.Scaffolder = &initScaffolder{}
