0bcd9e30013af5e99e301e69d1506246e0f5f080
Catherine Chan-Tse <cchantse@redhat.com>
2023-04-11 18:53:09 -0400

    Switch to ubi-micro:8.7
    Signed-off-by: Catherine Chan-Tse <cchantse@redhat.com>

--- pkg/plugins/hybrid/v1alpha/scaffolds/internal/templates/dockerfile.go
+++ pkg/plugins/hybrid/v1alpha/scaffolds/internal/templates/dockerfile.go
@@ -59,7 +59,7 @@ COPY controllers/ controllers/
 # Build
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o manager main.go
 
-FROM registry.access.redhat.com/ubi8/ubi-minimal:8.4
+FROM registry.access.redhat.com/ubi8/ubi-micro:8.7
 
 ENV HOME=/opt/helm \
     USER_NAME=helm \
