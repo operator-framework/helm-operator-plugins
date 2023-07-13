d771a95030597608c6a0d902bbeb5086b2276c54
Ludovic Cleroux <ludydoo@gmail.com>
2023-07-13 12:33:51 +0200

    PR Comments

--- pkg/reconciler/internal/updater/updater.go
+++ pkg/reconciler/internal/updater/updater.go
@@ -18,13 +18,13 @@ package updater
 
 import (
 	"context"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/util/wait"
 
 	"helm.sh/helm/v3/pkg/release"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/util/retry"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
