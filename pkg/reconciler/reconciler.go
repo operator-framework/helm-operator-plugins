6a51ffc7d3260674e4642b084f3daa703d5d5a5c
Yury Kovalev <ykovalev@redhat.com>
2023-07-26 15:16:51 +0200

    Avoid duplicated irreconcilable status

--- pkg/reconciler/reconciler.go
+++ pkg/reconciler/reconciler.go
@@ -625,7 +625,6 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (res ctrl.
 		)
 		return ctrl.Result{}, err
 	}
-	u.UpdateStatus(updater.EnsureCondition(conditions.Irreconcilable(corev1.ConditionFalse, "", "")))
 
 	for _, h := range r.preHooks {
 		if err := h.Exec(obj, vals, log); err != nil {
@@ -651,7 +650,9 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (res ctrl.
 			return ctrl.Result{}, err
 		}
 	default:
-		return ctrl.Result{}, fmt.Errorf("unexpected release state: %s", state)
+		err := fmt.Errorf("unexpected release state: %s", state)
+		u.UpdateStatus(updater.EnsureCondition(conditions.Irreconcilable(corev1.ConditionTrue, conditions.ReasonReconcileError, err)))
+		return ctrl.Result{}, err
 	}
 
 	for _, h := range r.postHooks {
