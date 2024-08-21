f1c835285989f2eb888031afcbceeaeddf0ee9a0
Adam D. Cornett <adc@redhat.com>
2024-08-21 14:12:09 -0700

    removing hybrid helm plugin
    Signed-off-by: Adam D. Cornett <adc@redhat.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -16,13 +16,7 @@ limitations under the License.
 
 package version
 
-import (
-	"fmt"
-	"runtime/debug"
-	"strings"
-
-	"github.com/blang/semver/v4"
-)
+import "fmt"
 
 const (
 	Unknown    = "unknown"
@@ -30,89 +24,22 @@ const (
 )
 
 var (
-	GitVersion      = Unknown
-	GitCommit       = Unknown
-	ScaffoldVersion = Unknown
+	GitVersion = Unknown
+	GitCommit  = Unknown
 )
 
-func init() {
-	// If the ScaffoldVersion was not set during the
-	// build (e.g. if this module was imported by
-	// another), try to deduce the scaffold version
-	// from the binary's embedded build info.
-	if ScaffoldVersion == Unknown {
-		ScaffoldVersion = getScaffoldVersion()
-	}
+var Version = Context{
+	Name:    modulePath,
+	Version: GitVersion,
+	Commit:  GitCommit,
 }
 
-// getScaffoldVersion parses build info embedded in
-// the binary to deduce a tag version that is
-// appropriate to use when scaffolding files that
-// need to reference the most recent release from
-// this repository.
-//
-// This allows other projects to import and use the
-// helm plugin and have versions populated correctly
-// in the scaffolded Makefile, Dockerfile, etc.
-func getScaffoldVersion() string {
-	info, ok := debug.ReadBuildInfo()
-	if ok {
-		// Search the dependencies of the main module and
-		// if we find this module, return its most recent
-		// tag.
-		for _, m := range info.Deps {
-			if m == nil {
-				continue
-			}
-			if m.Path == modulePath {
-				return getMostRecentTag(*m)
-			}
-		}
-	}
-
-	// If there isn't build info or we couldn't
-	// find our module in the main module's deps,
-	// return Unknown.
-	return Unknown
+type Context struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+	Commit  string `json:"commit"`
 }
 
-// getMostRecentTag translates m into a version string
-// that corresponds to the tag at (or that precedes) the
-// commit referenced by the m's version, accounting
-// for any replacements.
-//
-// If it can't deduce a tag, it returns "unknown".
-func getMostRecentTag(m debug.Module) string {
-	// Unwind all of the replacements.
-	for m.Replace != nil {
-		m = *m.Replace
-	}
-
-	// We need to handle the possibility of a pseudo-version.
-	// See: https://golang.org/cmd/go/#hdr-Pseudo_versions
-	//
-	// We'll get the first segment and attempt to parse it as
-	// semver.
-	split := strings.Split(m.Version, "-")
-	sv, err := semver.Parse(strings.TrimPrefix(split[0], "v"))
-
-	// If the first segment was not a valid semver string,
-	// return Unknown.
-	if err != nil {
-		return Unknown
-	}
-
-	// If there were multiple segments, m.Version is
-	// a pseudo-version, in which case Go will have
-	// incremented the patch version. If the patch
-	// version is greater than zero (it should always
-	// be), we'll decrement it to get back to the
-	// previous tag.
-	//
-	// This is necessary to handle projects that
-	// import this project at untagged commits.
-	if len(split) > 1 && sv.Patch > 0 {
-		sv.Patch--
-	}
-	return fmt.Sprintf("v%s", sv.FinalizeVersion())
+func (vc *Context) String() string {
+	return fmt.Sprintf("%s <commit: %s>", vc.Version, vc.Commit)
 }
--- main.go
+++ main.go
@@ -17,58 +17,13 @@ limitations under the License.
 package main
 
 import (
-	"fmt"
 	"log"
-	"runtime"
 
-	kustomizev2 "sigs.k8s.io/kubebuilder/v3/pkg/plugins/common/kustomize/v2"
-
-	"github.com/spf13/cobra"
-	"sigs.k8s.io/kubebuilder/v3/pkg/cli"
-	config "sigs.k8s.io/kubebuilder/v3/pkg/config/v3"
-	"sigs.k8s.io/kubebuilder/v3/pkg/model/stage"
-	"sigs.k8s.io/kubebuilder/v3/pkg/plugin"
-
-	golangv4 "sigs.k8s.io/kubebuilder/v3/pkg/plugins/golang/v4"
-
-	"github.com/operator-framework/helm-operator-plugins/internal/cmd/hybrid-operator/run"
-	"github.com/operator-framework/helm-operator-plugins/internal/version"
-	pluginv1alpha "github.com/operator-framework/helm-operator-plugins/pkg/plugins/hybrid/v1alpha"
+	"github.com/operator-framework/helm-operator-plugins/internal/cmd"
 )
 
 func main() {
-	commands := []*cobra.Command{
-		run.NewCmd(),
-	}
-	c, err := cli.New(
-		cli.WithCommandName("helm-operator"),
-		cli.WithVersion(getVersion()),
-		cli.WithPlugins(
-			getHybridPlugin(),
-			golangv4.Plugin{},
-		),
-		cli.WithDefaultProjectVersion(config.Version),
-		cli.WithDefaultPlugins(config.Version, getHybridPlugin()),
-		cli.WithExtraCommands(commands...),
-	)
-	if err != nil {
+	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
-
-	if err := c.Run(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getVersion() string {
-	return fmt.Sprintf("helm-operator version: %q, commit: %q, go version: %q, GOOS: %q, GOARCH: %q\n",
-		version.GitVersion, version.GitCommit, runtime.Version(), runtime.GOOS, runtime.GOARCH)
-}
-
-func getHybridPlugin() plugin.Bundle {
-	hybridBundle, _ := plugin.NewBundleWithOptions(plugin.WithName("hybrid"),
-		plugin.WithVersion(plugin.Version{Number: 1, Stage: stage.Alpha}),
-		plugin.WithPlugins(kustomizev2.Plugin{}, pluginv1alpha.Plugin{}))
-
-	return hybridBundle
 }
